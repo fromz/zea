05448797e6f9cea3755c72aaadeb9ff6ed5eae97
Kieran McDougall <kieran_mcdougall@technologyonecorp.com>
2022-11-24 03:24:32 +0000

    Added NATS exporter

---
+++ exporters/nats/README.md
@@ -0,0 +1,19 @@
+# NATS Exporter
+
+## Publish jar
+
+Publishing the JAR is handled by a github workflow. 
+
+### Republish jar with same tag
+
+Short form, including amending existing commit:
+```bash
+git add . && git commit --no-edit --amend && git tag 0.1.0-ALPHA-1 && git push --force --tags
+```
+
+Long form:
+```bash
+git tag -d 1.0.0-ALPHA-1
+git tag 1.0.0-ALPHA-1
+git push --force --tags
+```
---
+++ exporters/nats/src/io/camunda/zeebe/exporters/nats/NatsConfiguration.java
@@ -0,0 +1,27 @@
+package io.camunda.zeebe.exporters.nats;
+
+public class NatsConfiguration {
+    /**
+     * The URL of the Event Store database REST endpoint
+     */
+    String url = "http://localhost:2113";
+
+    /**
+     * The name of the stream in Event Store. This will automatically be created
+     * in Event Store when we first post data to it.
+     */
+    String subject = "zeebe";
+
+    /**
+     * To configure the amount of records, which has to be reached before the
+     * records are exported to the database. Only counts the records which are
+     * in the end actually exported.
+     */
+    int batchSize = 100;
+
+    /**
+     * To configure the time in milliseconds, when the batch should be executed
+     * regardless whether the batch size was reached or not.
+     */
+    int batchTimeMilli = 300;
+}
